45d18e9380a2c6b374d6eddf202c6751d052ab97
Martin Leone <martinnikolaslll@gmail.com>
2024-09-21 11:34:05 -0400

    Create displayEmployeeShifts function

--- Employee_Shift_Management_System.js
+++ Employee_Shift_Management_System.js
@@ -5,3 +5,27 @@ const employees = [
     {name: 'David', shifts: [{ day: 'Monday', hours: 8 }] },
     {name: 'Emily', shifts: [{ day: 'Friday', hours: 8 }] }
 ];
+
+//Task 2: Create a Function to Display Employee Shift Details
+
+function displayEmployeeShifts(employees) {
+    employees.forEach(employee => {
+        console.log(`Employee Name: ${employee.name}`);
+
+        if (employee.shifts && employee.shifts.length > 0 ) {
+            console.log('Assigned Shifts:'); //ensures employees actually have shifts and hours
+
+            employee.shifts.forEach(shifts => {
+                console.log(`Day: ${shifts.day}`); 
+                console.log(`Hours: ${shifts.hours}`);
+                //displays what days and hours employees are working
+            });
+        } else {
+            console.log(`No assigned Shifts for ${employees.name}`); //displays if an employee doesn't have hours assigned
+        }
+    });
+
+}
+
+//displays displayEmployeeShifts function
+displayEmployeeShifts(employees);
